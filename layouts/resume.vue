74499f8437e8982bc77bbc6eae64fea91e04f032
Guille <guiman.uy@gmail.com>
2022-07-18 16:43:54 -0300

    Design resume

--- layouts/resume.vue
+++ layouts/resume.vue
@@ -1,9 +1,9 @@
 <template>
   <main
-    class="font-firago hyphens-manual max-w-7xl mx-auto print:bg-white dark:bg-gradient-to-t dark:from-black dark:to-[#020202] dark:via-[#030120]"
+    class="font-firago hyphens-manual print:bg-white dark:bg-gradient-to-t dark:from-black dark:to-[#020202] dark:via-[#030120]"
   >
     <NavBar menu-text="SC" :menu-items="menuItems" />
-    <div class="print:bg-white print:px-0 md:px-10 md:py-4">
+    <div class="max-w-7xl mx-auto print:bg-white print:px-0 md:px-10 md:py-4">
       <slot />
     </div>
   </main>
